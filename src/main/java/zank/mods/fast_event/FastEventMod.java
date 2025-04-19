57170e6bcc89519f5d1b019bd21e8d3d6b486c87
ZZZank <3410764033@qq.com>
2025-04-19 13:22:03 +0800

    bare bone

---
+++ src/main/java/zank/mods/fast_event/FastEventMod.java
@@ -0,0 +1,11 @@
+package zank.mods.fast_event;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * @author ZZZank
+ */
+@Mod(FastEventMod.MOD_ID)
+public class FastEventMod {
+    public static final String MOD_ID = "fast_event";
+}
