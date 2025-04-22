7034294cf752c4789623b293c75953bf02ff6511
ZZZank <3410764033@qq.com>
2025-04-22 09:13:26 +0800

    back to 1.18.2
    This reverts commit cd223d6774144e9ad841d3d67b06c5a9bf2e3c2f.

--- src/main/java/zank/mods/fast_event/mixin/MixinASMEventHandler.java
+++ src/main/java/zank/mods/fast_event/mixin/MixinASMEventHandler.java
@@ -1,11 +1,16 @@
 package zank.mods.fast_event.mixin;
 
 import net.minecraftforge.eventbus.ASMEventHandler;
-import net.minecraftforge.eventbus.IEventListenerFactory;
 import net.minecraftforge.eventbus.api.IEventListener;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import zank.mods.fast_event.DummyHandler;
 import zank.mods.fast_event.EventListenerFactory;
 
 import java.lang.reflect.Method;
@@ -16,8 +21,18 @@ import java.lang.reflect.Method;
 @Mixin(value = ASMEventHandler.class, remap = false)
 public class MixinASMEventHandler {
 
-    @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/eventbus/IEventListenerFactory;create(Ljava/lang/reflect/Method;Ljava/lang/Object;)Lnet/minecraftforge/eventbus/api/IEventListener;"))
-    private IEventListener removeClassWrapperGen(IEventListenerFactory instance, Method method, Object target) {
-        return EventListenerFactory.createRawListener(method, target);
+    @Mutable
+    @Shadow
+    @Final
+    private IEventListener handler;
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void actualEventHandlerInit(Object target, Method method, boolean isGeneric, CallbackInfo ci) {
+        this.handler = EventListenerFactory.createRawListener(method, target);
+    }
+
+    @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/eventbus/ASMEventHandler;createWrapper(Ljava/lang/reflect/Method;)Ljava/lang/Class;"))
+    private Class<?> removeClassWrapperGen(ASMEventHandler instance, Method callback) {
+        return DummyHandler.class;
     }
 }
