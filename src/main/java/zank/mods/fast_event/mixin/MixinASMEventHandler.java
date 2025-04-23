9c0a55b7a067a27ffaabd1502495d849b5edb86f
ZZZank <3410764033@qq.com>
2025-04-23 16:20:32 +0800

    provide lookup manually
    (cherry picked from commit 165f0dd4e41f0b253bb240a43d19f4469ec99af2)

--- src/main/java/zank/mods/fast_event/mixin/MixinASMEventHandler.java
+++ src/main/java/zank/mods/fast_event/mixin/MixinASMEventHandler.java
@@ -8,6 +8,8 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import zank.mods.fast_event.EventListenerFactory;
 
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
 /**
@@ -16,8 +18,11 @@ import java.lang.reflect.Method;
 @Mixin(value = ASMEventHandler.class, remap = false)
 public class MixinASMEventHandler {
 
+    @Unique
+    private static final MethodHandles.Lookup fastEvent$LOOKUP = MethodHandles.lookup();
+
     @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/eventbus/IEventListenerFactory;create(Ljava/lang/reflect/Method;Ljava/lang/Object;)Lnet/minecraftforge/eventbus/api/IEventListener;"))
     private IEventListener removeClassWrapperGen(IEventListenerFactory instance, Method method, Object target) {
-        return EventListenerFactory.createRawListener(method, target);
+        return EventListenerFactory.createRawListener(fastEvent$LOOKUP, method, target);
     }
 }
