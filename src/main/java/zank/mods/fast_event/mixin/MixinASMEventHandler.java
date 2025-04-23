165f0dd4e41f0b253bb240a43d19f4469ec99af2
ZZZank <3410764033@qq.com>
2025-04-23 16:20:32 +0800

    provide lookup manually

--- src/main/java/zank/mods/fast_event/mixin/MixinASMEventHandler.java
+++ src/main/java/zank/mods/fast_event/mixin/MixinASMEventHandler.java
@@ -2,10 +2,7 @@ package zank.mods.fast_event.mixin;
 
 import net.minecraftforge.eventbus.ASMEventHandler;
 import net.minecraftforge.eventbus.api.IEventListener;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -13,6 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import zank.mods.fast_event.DummyHandler;
 import zank.mods.fast_event.EventListenerFactory;
 
+import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
@@ -22,6 +20,9 @@ import java.lang.reflect.Method;
 @Mixin(value = ASMEventHandler.class, remap = false)
 public class MixinASMEventHandler {
 
+    @Unique
+    private static final MethodHandles.Lookup fastEvent$LOOKUP = MethodHandles.lookup();
+
     @Mutable
     @Shadow
     @Final
@@ -29,7 +30,7 @@ public class MixinASMEventHandler {
 
     @Inject(method = "<init>", at = @At("RETURN"))
     private void actualEventHandlerInit(Object target, Method method, boolean isGeneric, CallbackInfo ci) {
-        this.handler = EventListenerFactory.createRawListener(method, target);
+        this.handler = EventListenerFactory.createRawListener(fastEvent$LOOKUP, method, target);
     }
 
     @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/eventbus/ASMEventHandler;createWrapper(Ljava/lang/reflect/Method;)Ljava/lang/Class;"))
